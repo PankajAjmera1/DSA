de4cd9f3a0f6d2aa125e2c93712d2113ded808ed
Pankaj Ajmera <coderajmera@gmail.com>
2024-04-11 16:26:32 +0530

    Recursion

--- Recursion/pi.java
+++ Recursion/pi.java
@@ -9,9 +9,18 @@ public class pi{
         printIncreasing(n-1); //1,2,3,4....n-1
         System.out.println(n);
     }
+    static void printDecreasing(int n){
+        if(n==1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1); 
+    }
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
-        printIncreasing(n);
+       // printIncreasing(n);
+        printDecreasing(n);
     }
 }
